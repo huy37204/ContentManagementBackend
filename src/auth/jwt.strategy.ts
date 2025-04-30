2032f3b42710b1f0443570ba3916adf1528ab71e
huy37204 <huy37204@gmail.com>
2025-05-01 01:35:23 +0700

    fix ts error in jwt.strategy

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,6 +1,6 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
-import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ExtractJwt, Strategy, StrategyOptionsWithRequest } from 'passport-jwt';
 import { Request } from 'express';
 import { RedisService } from 'src/common/services/redis.service';
 
@@ -11,18 +11,17 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: process.env.JWT_SECRET,
       passReqToCallback: true, // cần để lấy request trong validate()
-    });
+    } as StrategyOptionsWithRequest);
   }
 
   async validate(req: Request, payload: any) {
     const token = ExtractJwt.fromAuthHeaderAsBearerToken()(req);
 
-    // Kiểm tra token trong Redis (ví dụ lưu dưới key: token:<userId>)
     const cachedToken = await this.redisService.get(`token:${payload.sub}`);
     if (!cachedToken || cachedToken !== token) {
       throw new UnauthorizedException('Token is revoked or invalid');
     }
 
-    return { userId: payload.sub, role: payload.role }; // inject vào req.user
+    return { userId: payload.sub, role: payload.role };
   }
 }
