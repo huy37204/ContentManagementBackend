c19667c0d69d0555fb5e2d5245403f7427b32d15
huy37204 <huy37204@gmail.com>
2025-05-04 17:19:30 +0700

    mock dependency of service/controller

--- src/contents/contents.service.spec.ts
+++ src/contents/contents.service.spec.ts
@@ -1,12 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ContentsService } from './contents.service';
+import { getModelToken } from '@nestjs/mongoose';
+import { ContentsGateway } from './contents.gateway';
 
 describe('ContentsService', () => {
   let service: ContentsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ContentsService],
+      providers: [
+        ContentsService,
+        {
+          provide: getModelToken('Content'),
+          useValue: {}, // hoặc mock bằng jest.fn()
+        },
+        {
+          provide: ContentsGateway,
+          useValue: {}, // nếu có phương thức cần mock, dùng jest.fn()
+        },
+      ],
     }).compile();
 
     service = module.get<ContentsService>(ContentsService);
