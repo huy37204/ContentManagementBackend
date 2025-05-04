cef719167cdf06da140b2a8524522516bdaf3646
huy37204 <huy37204@gmail.com>
2025-05-04 17:23:16 +0700

    mock dependency of service/controller

--- src/contents/contents.controller.spec.ts
+++ src/contents/contents.controller.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ContentsController } from './contents.controller';
+import { ContentsService } from './contents.service';
+import { S3Service } from 'src/common/services/s3.service';
 
 describe('ContentsController', () => {
   let controller: ContentsController;
@@ -7,6 +9,16 @@ describe('ContentsController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ContentsController],
+      providers: [
+        {
+          provide: ContentsService,
+          useValue: {}, // hoặc jest.fn() nếu cần
+        },
+        {
+          provide: S3Service,
+          useValue: {},
+        },
+      ],
     }).compile();
 
     controller = module.get<ContentsController>(ContentsController);
